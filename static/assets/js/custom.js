5392830f26c1e09b1c330e42fc80fce8a53088ad
Clayton D <cod5000@gmail.com>
2021-01-23 13:48:49 -0500

    Owl carousel tweak

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -30,22 +30,25 @@ Version: 1.0
     objowlcarousel.owlCarousel({
       responsive: {
         0: {
-          items: 1,
-
-
+          items: 1
         },
-        767: {
-          items: 3,
 
+        600: {
+          items: 3
         },
-        1000: {
-          items: 3,
+
+        1024: {
+          items: 4
+        },
+
+        1366: {
+          items: 4
         }
       },
       loop: true,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      autoplayTimeout: 4000,
+      // autoplay: true,
+      // autoplaySpeed: 3000,
+      // autoplayTimeout: 4000,
       autoplayHoverPause: true,
       nav: true,
       navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
